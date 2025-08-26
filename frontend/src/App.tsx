818bd977f6f1b23775896d838b4fe74e467840f3
huniversal <hunivers0523@naver.com>
2025-08-27 02:22:39 +0900

    refactor: 게시글 작성 페이지 라우트 및 링크 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import PostListPage from './pages/post/PostListPage';
+import PostCreatePage from './pages/post/PostCreatePage';
 
 // 홈 페이지 컴포넌트 생성
 const HomePage = () => {
@@ -22,11 +23,13 @@ function App() {
         <nav style={{ marginBottom: '20px' }}>
           <Link to="/" style={{ marginRight: '10px' }}>홈</Link>
           <Link to="/posts">게시글 목록</Link>
+          <Link to="/posts/create">게시글 작성</Link>
         </nav>
         
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/posts" element={<PostListPage />} />
+          <Route path="/posts/create" element={<PostCreatePage />} />
         </Routes>
       </div>
     </Router>
