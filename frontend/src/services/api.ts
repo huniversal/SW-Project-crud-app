4b3b6e25aef717a9691fb0b5957ad1260725a9bb
huniversal <hunivers0523@naver.com>
2025-08-26 21:30:10 +0900

    feat: axios 및 인터셉터 세팅

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+// import { ApiResponse, Post } from '../types/index';
+
+// axios 기본 설정
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 요청 인터셉터
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
